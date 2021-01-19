f940ca580598525dff0e9ada56fc9f39a17a2893
gmartinezgh <gustavo.martinez.gonzalez@gmail.com>
2021-01-19 01:13:37 +0100

    Scraping vivino top rated wines of users. Removing obsolete graph.py

--- graph.py
+++
@@ -1,7 +0,0 @@
-from graphframes import *
-
-users = sqlContext.createDataFrame(df.drop(["image", "background_image", "address", "statistics"], axis = 1))
-relationships = sqlContext.createDataFrame(df[["id", "following"]].explode("following").rename(columns={"id": "src", "following":"dst"}).append(
-  df[["id", "followers"]].explode("followers").rename(columns={"id": "dst", "followers":"src"})))
-
-g = GraphFrame(users, relationships)
